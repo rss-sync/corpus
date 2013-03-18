5064be58316df0d8f7d9af7059958b28e64b3b85
David Kendal <dpk@obquire.default.trilby.uk0.bigv.io>
2013-03-18 15:33:05 +0000

    initial commit

---
+++ download.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys, os, urllib.request, re
+from datetime import datetime
+
+FS = re.compile(r'\s+')
+
+with open('sources', 'r', encoding='utf-8') as f:
+  for line in f:
+    line = line.rstrip()
+    name, url = FS.split(line)
+
+    if not os.path.isdir(name): os.mkdir(name)
+
+    print(name)
+    with urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (rss-sync corpus gatherer)'})) as source:
+      destname = ("%s/%s.xml" % (name, datetime.utcnow().strftime('%Y%m%dT%H%M%S')))
+      with open(destname, 'wb') as dest:
+        dest.write(source.read())
+        dest.flush()
+
+        
