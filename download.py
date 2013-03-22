02ec6840255de79e78ece43f49aeffa4209ddc2a
David Kendal <dpk@obquire.default.trilby.uk0.bigv.io>
2013-03-22 16:13:13 +0000

    handle failures better

--- download.py
+++ download.py
@@ -13,12 +13,13 @@ with open('sources', 'r', encoding='utf-8') as f:
     if not os.path.isdir(name): os.mkdir(name)
 
     print("%-16s" % name, end=''); sys.stdout.flush()
-    with urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (rss-sync corpus gatherer)'})) as source:
-      destname = ("%s/%s.xml" % (name, datetime.utcnow().strftime('%Y%m%dT%H%M%S')))
-      with open(destname, 'wb') as dest:
-        feedsrc = source.read()
-        dest.write(feedsrc)
-        print("%d bytes" % len(feedsrc))
-        dest.flush()
-
-        
+    try:
+      with urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (rss-sync corpus gatherer)'})) as source:
+        destname = ("%s/%s.xml" % (name, datetime.utcnow().strftime('%Y%m%dT%H%M%S')))
+        with open(destname, 'wb') as dest:
+          feedsrc = source.read()
+          dest.write(feedsrc)
+          print("%d bytes" % len(feedsrc))
+          dest.flush()
+    except:
+      print("FAILED! %s" % sys.exc_info()[0])
